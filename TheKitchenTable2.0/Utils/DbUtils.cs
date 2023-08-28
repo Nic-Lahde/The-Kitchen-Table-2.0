e319b75315ec1f41a277b55b6486d0f92e2a4b33
Nic <nic.lahde@gmail.com>
2023-08-28 09:48:14 -0500

    add DbUtils

---
+++ TheKitchenTable2.0/Utils/DbUtils.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheKitchenTable2._0.Utils
+{
+    public class Class
+    {
+    }
+}
